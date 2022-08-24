ef0d23095f8f53cc49bf35a803a3792f957106b5
JuanVLujan <110927379+JuanVLujan@users.noreply.github.com>
2022-08-24 13:00:11 -0500

    Add files via upload
    Eternal loop fixed

--- Cajero.java
+++ Cajero.java
@@ -24,44 +24,44 @@ public class Cajero{
         resta = cantidad;
             while(resta >= 100000)
             {
-                resta = cantidad - 100000;
+                resta = resta - 100000;
                 b6++; 
             }while(resta >= 50000){
-                resta = cantidad - 50000;
+                resta = resta - 50000;
                 b5++; 
             }while(resta >= 20000){
-                resta = cantidad - 20000;
+                resta = resta - 20000;
                 b4++; 
             }while(resta >= 10000){
-                resta = cantidad - 10000;
+                resta = resta - 10000;
                 b3++; 
             }while(resta >= 5000){
-                resta = cantidad - 5000;
+                resta = resta - 5000;
                 b2++; 
             }while(resta >= 2000){
-                resta = cantidad - 2000;
+                resta = resta - 2000;
                 b1++; 
             }while(resta >= 1000){
-                resta = cantidad - 1000;
+                resta = resta - 1000;
                 b0++;
             }
         if (b6 > 0){
-            acumulador_texto = acumulador_texto + (b6 + " billetes de 100.000.\n");
+            acumulador_texto = acumulador_texto + (b6 + " billete(s) de 100.000.\n");
         }if (b5 > 0){
-            acumulador_texto = acumulador_texto + (b5 + " billetes de 50.000.\n");
+            acumulador_texto = acumulador_texto + (b5 + " billete(s) de 50.000.\n");
         }if (b4 > 0){
-            acumulador_texto = acumulador_texto + (b4 + " billetes de 20.000.\n");
+            acumulador_texto = acumulador_texto + (b4 + " billete(s) de 20.000.\n");
         }if (b3 > 0){
-            acumulador_texto = acumulador_texto + (b3 + " billetes de 10.000.\n");
+            acumulador_texto = acumulador_texto + (b3 + " billete(s) de 10.000.\n");
         }if (b2 > 0){
-            acumulador_texto = acumulador_texto + (b2 + " billetes de 5.000.\n");
+            acumulador_texto = acumulador_texto + (b2 + " billete(s) de 5.000.\n");
         }if (b1 > 0){
-            acumulador_texto = acumulador_texto + (b1 + " billetes de 2.000.\n");
+            acumulador_texto = acumulador_texto + (b1 + " billete(s) de 2.000.\n");
         }if (b0 > 0){
-            acumulador_texto = acumulador_texto + (b0 + " billetes de 1.000.\n");
+            acumulador_texto = acumulador_texto + (b0 + " billete(s) de 1.000.\n");
         }
         JOptionPane.showMessageDialog(null,"Se requieren: \n" + acumulador_texto,"ALGORITO CAJERO",1);
-        sw = Integer.parseInt(JOptionPane.showInputDialog(null,"¿Desea realizar un nuevo conteo?","ALGORITO CAJERO",3));    
+        sw = Integer.parseInt(JOptionPane.showInputDialog(null,"¿Desea realizar un nuevo conteo?\n1- SI\nOTRO- NO","ALGORITO CAJERO",3));    
      }while(sw==1);
     }
     
